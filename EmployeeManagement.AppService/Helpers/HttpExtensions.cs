46ebd59d716487306dd304dc77bc9014f1eeb5c2
Oremichen Jesumeichen <joremichen@coure-tech.com>
2021-09-11 11:19:41 +0100

    update pagenationheader

---
+++ EmployeeManagement.AppService/Helpers/HttpExtensions.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManagement.Core;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace EmployeeManagement.AppService.Helpers
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse response, int currentPage, int itemsPerpage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PagenationHeader(currentPage, itemsPerpage, totalItems, totalPages);
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+
+        }
+    }
+}
---
+++ EmployeeManagement.Core/PagenationHeader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement.Core
+{
+    public class PagenationHeader
+    {
+        public PagenationHeader(int currentPage, int itemsPerpage, int totalItems, int totalPages)
+        {
+            CurrentPage = currentPage;
+            ItemsPerPage = itemsPerpage;
+            TotalItems = totalItems;
+            TotalPages = totalPages;
+        }
+
+        public int CurrentPage { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
